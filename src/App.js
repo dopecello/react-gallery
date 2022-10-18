9330d9eabe465c7649db1117e51042002c3d7132
dopecello <williamlocke.cello@gmail.com>
2022-10-18 18:11:01 -0400

    created gallery component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from "react";
 import "./App.css";
 import About from "./components/About";
 import Nav from "./components/Nav";
+import Gallery from "./components/Gallery";
 
 const App = () => {
   return (
     <div>
       <Nav></Nav>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
     </div>
   );
-}
+};
 
 export default App;

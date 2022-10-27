9ab05c52d4f7847344faa1a6cc0de30f758f5ee5
dopecello <williamlocke.cello@gmail.com>
2022-10-27 17:18:45 -0400

    tested contact component with success

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -36,7 +36,7 @@ function ContactForm() {
 
   return (
     <section>
-      <h1>Contact Me</h1>
+      <h1 data-testid="h1tag">Contact Me</h1>
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
@@ -70,7 +70,7 @@ function ContactForm() {
                 <p className="error-text">{errorMessage}</p>
             </div>
         )}
-        <button type="submit">Submit</button>
+        <button type="submit" data-testid="button">Submit</button>
       </form>
     </section>
   );
--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -3,27 +3,59 @@ import { render, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Nav from "..";
 
+const categories = [
+  { name: 'portraits', description: 'Portraits of people in my life' }
+]
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
+
 describe("Nav component", () => {
-  it("renders", () => {
-    render(<Nav />);
-  });
+  it('renders', () => {
+    render(<Nav
+      categories={categories}
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockCurrentCategory}
+      contactSelected={mockContactSelected}
+      setContactSelected={mockSetContactSelected}
+    />);
+  })
 
   it("matches snapshot", () => {
-    const { asFragment } = render(<Nav />);
+    const { asFragment } =render(<Nav
+    categories={categories}
+    setCurrentCategory={mockSetCurrentCategory}
+    currentCategory={mockCurrentCategory}
+    contactSelected={mockContactSelected}
+    setContactSelected={mockSetContactSelected}
+  />);
     expect(asFragment()).toMatchSnapshot();
   });
 });
 
 describe("emoji is visible", () => {
   it("inserts emoji into the h2", () => {
-    const { getByLabelText } = render(<Nav />);
+    const { getByLabelText } =render(<Nav
+    categories={categories}
+    setCurrentCategory={mockSetCurrentCategory}
+    currentCategory={mockCurrentCategory}
+    contactSelected={mockContactSelected}
+    setContactSelected={mockSetContactSelected}
+  />);
     expect(getByLabelText("camera")).toHaveTextContent("📸");
   });
 });
 
 describe("links are visible", () => {
   it("inserts text into the links", () => {
-    const { getByTestId } = render(<Nav />);
+    const { getByTestId } =render(<Nav
+    categories={categories}
+    setCurrentCategory={mockSetCurrentCategory}
+    currentCategory={mockCurrentCategory}
+    contactSelected={mockContactSelected}
+    setContactSelected={mockSetContactSelected}
+  />);
     expect(getByTestId("link")).toHaveTextContent("React Gallery");
     expect(getByTestId("about")).toHaveTextContent("About Me");
   });
